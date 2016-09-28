6bc81516e612a1cc9067c50bbf579a1dfdf35628
ststeiger <quandary82@hailmail.net>
2016-09-28 17:30:14 +0200

    Read ReportServer Auth-cookie

--- vbConsoleTestApplication/Module1.vb
+++ vbConsoleTestApplication/Module1.vb
@@ -12,6 +12,12 @@ End Class ' User
 Module Module1
 
 
+    Public Function GetCookieValue() As String
+        Dim authCookie As System.Web.HttpCookie = System.Web.HttpContext.Current.Request.Cookies("sqlAuthCookie")
+        Return authCookie.Value
+    End Function
+
+
     ' http://stackoverflow.com/questions/1668353/how-can-i-generate-a-cryptographically-secure-pseudorandom-number-in-c
     Public Function GenerateRandomKey(byteCount As Integer) As Byte()
         Dim tokenData As Byte() = New Byte(byteCount - 1) {}
@@ -116,4 +122,4 @@ Module Module1
         System.Console.ReadKey()
     End Sub ' Main 
 
-End Module 
+End Module
