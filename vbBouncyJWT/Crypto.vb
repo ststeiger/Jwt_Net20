8b328447eb035766733d6f55d73897b7b48d1399
ststeiger <quandary82@hailmail.net>
2016-10-04 09:53:34 +0200

    Added missing files

---
+++ vbBouncyJWT/Crypto.vb
@@ -0,0 +1,121 @@
+﻿
+Namespace BouncyJWT
+
+
+    Public Class Crypto
+
+
+        ' GenerateRsaKeyPair(1024)
+        Public Shared Function GenerateRsaKeyPair(strength As Integer) As Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair
+            Dim gen As New Org.BouncyCastle.Crypto.Generators.RsaKeyPairGenerator()
+            Dim secureRandom As New Org.BouncyCastle.Security.SecureRandom(New Org.BouncyCastle.Crypto.Prng.CryptoApiRandomGenerator())
+            Dim keyGenParam As New Org.BouncyCastle.Crypto.KeyGenerationParameters(secureRandom, strength)
+
+            gen.Init(keyGenParam)
+
+            Dim kp As Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair = gen.GenerateKeyPair()
+            Return kp
+        End Function ' GenerateRsaKeyPair 
+
+
+        Public Shared Function GenerateRandomRsaPrivateKey(strength As Integer) As String
+            Dim gen As New Org.BouncyCastle.Crypto.Generators.RsaKeyPairGenerator()
+            Dim secureRandom As New Org.BouncyCastle.Security.SecureRandom(New Org.BouncyCastle.Crypto.Prng.CryptoApiRandomGenerator())
+            Dim keyGenParam As New Org.BouncyCastle.Crypto.KeyGenerationParameters(secureRandom, strength)
+
+            gen.Init(keyGenParam)
+
+            Dim kp As Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair = gen.GenerateKeyPair()
+
+            Return StringifyAsymmetricKey(kp.Private)
+        End Function ' GenerateRsaKeyPair 
+
+
+        Public Shared Sub WritePrivatePublic(keyPair As Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair)
+            Dim privateKey As String = Nothing
+            Dim publicKey As String = Nothing
+            
+            ' id_rsa
+            Using textWriter As System.IO.TextWriter = New System.IO.StringWriter()
+                Dim pemWriter As New Org.BouncyCastle.OpenSsl.PemWriter(textWriter)
+                pemWriter.WriteObject(keyPair.[Private])
+                pemWriter.Writer.Flush()
+
+                privateKey = textWriter.ToString()
+            End Using ' textWriter 
+
+            ' id_rsa.pub
+            Using textWriter As System.IO.TextWriter = New System.IO.StringWriter()
+                Dim pemWriter As New Org.BouncyCastle.OpenSsl.PemWriter(textWriter)
+                pemWriter.WriteObject(keyPair.[Public])
+                pemWriter.Writer.Flush()
+
+                publicKey = textWriter.ToString()
+            End Using ' textWriter
+
+            System.Console.WriteLine(privateKey)
+            System.Console.WriteLine(publicKey)
+        End Sub ' WritePrivatePublic
+
+
+        Public Shared Function StringifyAsymmetricKey(privateOrPublicKey As Org.BouncyCastle.Crypto.AsymmetricKeyParameter) As String
+            Dim key As String = Nothing
+
+            Using textWriter As System.IO.TextWriter = New System.IO.StringWriter()
+                Dim pemWriter As New Org.BouncyCastle.OpenSsl.PemWriter(textWriter)
+                pemWriter.WriteObject(privateOrPublicKey)
+                pemWriter.Writer.Flush()
+
+                key = textWriter.ToString()
+            End Using ' textWriter 
+
+            Return key
+        End Function
+
+
+        Public Shared Function ReadPrivateKey(privateKey As String) As Org.BouncyCastle.Crypto.AsymmetricKeyParameter
+            Dim keyPair As Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair = Nothing
+
+            Using reader As System.IO.TextReader = New System.IO.StringReader(privateKey)
+                Dim pemReader As New Org.BouncyCastle.OpenSsl.PemReader(reader)
+
+                Dim obj As Object = pemReader.ReadObject()
+
+                If TypeOf obj Is Org.BouncyCastle.Crypto.AsymmetricKeyParameter Then
+                    Throw New System.ArgumentException("The given privateKey is a public key, not a privateKey...", "privateKey")
+                End If
+
+                If Not (TypeOf obj Is Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair) Then
+                    Throw New System.ArgumentException("The given privateKey is not a valid assymetric key.", "privateKey")
+                End If
+
+                keyPair = DirectCast(obj, Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair)
+            End Using ' reader 
+
+            ' Org.BouncyCastle.Crypto.AsymmetricKeyParameter priv = keyPair.Private;
+            ' Org.BouncyCastle.Crypto.AsymmetricKeyParameter pub = keyPair.Public;
+
+            ' Note: 
+            ' cipher.Init(False, key)
+            ' !!!
+
+            Return keyPair.Private
+        End Function ' ReadPrivateKey
+
+
+        Public Function ReadPublicKeyFile(pemFilename As String) As Org.BouncyCastle.Crypto.AsymmetricKeyParameter
+            Dim keyParameter As Org.BouncyCastle.Crypto.AsymmetricKeyParameter = Nothing
+
+            Using streamReader As System.IO.StreamReader = System.IO.File.OpenText(pemFilename)
+                Dim pemReader As New Org.BouncyCastle.OpenSsl.PemReader(streamReader)
+                keyParameter = DirectCast(pemReader.ReadObject(), Org.BouncyCastle.Crypto.AsymmetricKeyParameter)
+            End Using ' fileStream 
+
+            Return keyParameter
+        End Function ' ReadPublicKey 
+
+
+    End Class
+
+
+End Namespace ' BouncyJWT 
---
+++ vbConsoleTestApplication/TestBouncyJWT.vb
@@ -0,0 +1,30 @@
+﻿
+Public Class TestBouncyJWT
+
+
+    Public Shared Sub Test()
+        Dim pubKey As String = "-----BEGIN PUBLIC KEY-----" & vbCr & vbLf & "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCYk9VtkOHIRNCquRqCl9bbFsGw" & vbCr & vbLf & "HdJJPIoZENwcHYBgnqVoEa5SJs8ddkNNna6M+Gln2n4S/G7Mu+Cz0cQg06Ru8919" & vbCr & vbLf & "hYWGWdyVumAGgJwMEKAzUj9651Y6AAOcAM0qX/f0DrLlUAZFy+64L8kVjuCyFdti" & vbCr & vbLf & "5d3yaGnFM+Xw/4fcLwIDAQAB" & vbCr & vbLf & "-----END PUBLIC KEY-----" & vbCr & vbLf
+        Dim privKey As String = "-----BEGIN RSA PRIVATE KEY-----" & vbCr & vbLf & "MIICXQIBAAKBgQCYk9VtkOHIRNCquRqCl9bbFsGwHdJJPIoZENwcHYBgnqVoEa5S" & vbCr & vbLf & "Js8ddkNNna6M+Gln2n4S/G7Mu+Cz0cQg06Ru8919hYWGWdyVumAGgJwMEKAzUj96" & vbCr & vbLf & "51Y6AAOcAM0qX/f0DrLlUAZFy+64L8kVjuCyFdti5d3yaGnFM+Xw/4fcLwIDAQAB" & vbCr & vbLf & "AoGAGyDZ51/FzUpzAY/k50hhEtZSfOJog84ITdmiETurmkJK7ZyLLp8o3zeqUtAQ" & vbCr & vbLf & "+46liyodlXmdp7hWBRLseNu4lh1gQGYj4/fH2BT75/zFngaTdz7pANKq6Y5IOHg0" & vbCr & vbLf & "C1UatzmuSmDGk/l7g1gQyWo8dcwjrzvsGWBAFZ4QHy2OsE0CQQDNStOX0USyfgrZ" & vbCr & vbLf & "AkKOfs3paaxVB/SZTBaorcqo8nBX1Fx/rdpBTIezHuZQchF/BGpHLS7/yyve+jg/" & vbCr & vbLf & "dspR7XZdAkEAvkO10QFsDR1GJwVcUpG1LguznKqS7v6FscnpBFvfsf7UaqNHCGvY" & vbCr & vbLf & "Feau1EwekVRl77ZKUPhDQt7XFniBO40b+wJBALZnQ7Xi1H0bjJvgbC6b8Gzx3ZL3" & vbCr & vbLf & "rJcAiil5sVWHg9Yl88HmQMRAMVovnEfh8jW/QIbZWKciaGqIPK326DD/ImkCQQCC" & vbCr & vbLf & "k1OHQfOWuH15sCshG5B9Lliw7ztxu8mjL0+0xxypOpsrKC1KsUCWHz/iwO7FjGd8" & vbCr & vbLf & "8Nzl3svCa86vRDpk1T3bAkBWjvKigxbkpYPbayKwjeWTiS3YIg63N2WUaetFBAD2" & vbCr & vbLf & "Yrv+Utm12zi99pZNA5WCqO/UhN9poJdWaYqYYImYhH8N" & vbCr & vbLf & "-----END RSA PRIVATE KEY-----" & vbCr & vbLf
+
+
+        Dim key As New BouncyJWT.JwtKey() With {.RSA = privKey}
+        Dim arbitraryKey As New BouncyJWT.JwtKey() With {.RSA = BouncyJWT.Crypto.GenerateRandomRsaPrivateKey(1024)}
+
+        Dim token As String = BouncyJWT.JsonWebToken.Encode(New User(), key, BouncyJWT.JwtHashAlgorithm.RS256)
+
+        System.Console.WriteLine(token)
+        Dim thisUser As User = BouncyJWT.JsonWebToken.DecodeToObject(Of User)(token, key, True)
+        Dim wrongUser As User = BouncyJWT.JsonWebToken.DecodeToObject(Of User)(token, arbitraryKey, True)
+
+
+        System.Console.WriteLine(thisUser)
+        System.Console.WriteLine(wrongUser)
+
+
+        System.Console.WriteLine(System.Environment.NewLine)
+        System.Console.WriteLine(" --- Press any key to continue --- ")
+        System.Console.ReadKey()
+    End Sub
+
+
+End Class
